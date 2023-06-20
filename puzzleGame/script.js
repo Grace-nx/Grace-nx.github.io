d4a427cb49685fab7ac592cf8a3bca4678820c74
Grace-nx <126555178+Grace-nx@users.noreply.github.com>
2023-06-20 16:06:05 +0800

    Add files via upload

---
+++ puzzleGame/script.js
@@ -0,0 +1,116 @@
+
+function playerName(){
+var name = prompt("怎么称呼你呢？");
+alert("你好呀，" + name + "，欢迎来到这个测试小游戏！");
+}
+playerName()
+
+
+function result1(){
+        const move = document.getElementById("move").value;
+        if(move=="1"){
+          alert("常常把自己藏起来的人，很有神秘感，不容易接近。");
+        }
+        else if(move=="2"){
+          alert("很会关心别人的人。很容易发现身边有人不开心，不会很容易讲错话。");
+        }
+        else if(move=="3"){
+          alert("傻傻的人。怪怪的，想做什么就做什么，不过很可爱。");
+        }
+        else if(move=="4"){
+          alert("不会老是要人担心的人。让人很放心。");
+        }
+        else if(move=="5"){
+          alert("负责任、有领导能力的人，人们对你非常尊敬。");
+        }
+        else if (true) {
+          alert("请填写正确的排序（数字1~5）。")
+        }
+      }
+function result2(){
+        const sad = document.getElementById("sad").value;
+        if(sad=="1"){
+          alert("对很多事都要求很高的人，不过有很多的时候都太固执了。");
+        }
+        else if(sad=="2"){
+          alert("可以跟你讲道理、黑白之间分辨得很清楚的人。");
+        }
+        else if(sad=="3"){
+          alert("心思很细的人。很多时候伤心不会表现出来，不过大家其实都看得出来。");
+        }
+        else if(sad=="4"){
+          alert("会先想很多才做出选择的人。不想自己给人看扁，自尊心很强。");
+        }
+        else if(sad=="5"){
+          alert("外刚内柔的人。其实知道你的内心不是那么的坚强。");
+        }
+        else if (true) {
+          alert("请填写正确的排序（数字1~5）。")
+        }
+      }
+
+function result3(){
+        const pain = document.getElementById("pain").value;
+        if(pain=="1"){
+          alert("很需要被别人保护的人。");
+        }
+        else if(pain=="2"){
+          alert("不是那么容易接近的人。");
+        }
+        else if(pain=="3"){
+          alert("很关心身边人的人。");
+        }
+        else if(pain=="4"){
+          alert("很聪明又不会骄傲的人。");
+        }
+        else if(pain=="5"){
+          alert("很清楚自己想什么、要什么的人。");
+        }
+        else if (true) {
+          alert("请填写正确的排序（数字1~5）。")
+        }
+      }
+
+function result4(){
+        const angry = document.getElementById("angry").value;
+        if(angry=="1"){
+          alert("跟你很合拍的人，你和他想的东西是一样的，不用问便知道对方想要什么。");
+        }
+        else if(angry=="2"){
+          alert("不会很容易发脾气、懂得容忍、外刚内柔的人，有自己的性格。");
+        }
+        else if(angry=="3"){
+          alert("内心很可爱的人，猜不到他下一步会做什么。");
+        }
+        else if(angry=="4"){
+          alert("很细心的人，你需要什么他都有准备好。");
+        }
+        else if(angry=="5"){
+          alert("很智慧、能够管住你的人。");
+        }
+        else if (true) {
+          alert("请填写正确的排序（数字1~5）。")
+        }
+      }
+
+function result5(){
+        const nervous = document.getElementById("nervous").value;
+        if(nervous=="1"){
+          alert("一个很怕让别人看到自己是什么样的人，不太喜欢自己的性格。");
+        }
+        else if(nervous=="2"){
+          alert("孤独的人，很希望可以跟一大堆人在一起，不过很多时候不知道怎样跟别人沟通。");
+        }
+        else if(nervous=="3"){
+          alert("觉得朋友比天还重要的人，很珍惜身边的很多朋友，敢爱敢恨，不喜欢的人你就不在意。");
+        }
+        else if(nervous=="4"){
+          alert("直接的人，希望有更多人了解你，特别是喜欢的人。");
+        }
+        else if(nervous=="5"){
+          alert("不是很清楚自己想要什么的人。但是很幸运，你不会对很多东西有要求，认为开心过每一天最重要。");
+        }
+        else if (true) {
+          alert("请填写正确的排序（数字1~5）。")
+        }
+      }
